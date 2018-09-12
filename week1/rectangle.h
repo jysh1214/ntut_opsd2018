046d09f2370e3fad6d71d21575b0a0b5e6eafae7
jysh1214 <jyxemperor@gmail.com>
2018-09-12 15:29:12 +0800

    Create rectangle.h

---
+++ week1/rectangle.h
@@ -0,0 +1,34 @@
+class Rectangle
+{
+    public:
+        Rectangle(double length, double width):_l(length), _w(width)
+        {
+
+        }
+
+        double area() const
+        {
+            return _l * _w;
+        }
+
+    	double perimeter() const
+    	{
+    		return 2 * _l + 2 * _w;
+    	}
+
+        bool test()
+        {
+            if ((_l<=0)or(_w<=0))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    private:
+    	double _l;
+    	double _w;
+};
