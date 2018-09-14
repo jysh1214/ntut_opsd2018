666a2192607331f3644735c7cfda4cc2138666ae
jysh1214 <jyxemperor@gmail.com>
2018-09-14 16:49:41 +0800

    Update triangle.h

--- week1/triangle.h
+++ week1/triangle.h
@@ -3,7 +3,7 @@
 class Triangle
 {
     public:
-        Triangle(double a_side, double b_side, double c_side):_a(a_side), _b(b_side), _c(c_side)
+        Triangle(double x1, double y1, double x2, double y2, double x3, double y3):_x1(x1), _x2(x2), _x3(x3), _y1(y1), _y2(y2), _y3(y3)
         {
 
         }
@@ -15,11 +15,19 @@ class Triangle
 
     	double perimeter() const
     	{
+    		double _a = dis(_x1, _y1, _x2, _y2);
+    	    double _b = dis(_x1, _y1, _x3, _y3);
+    	    double _c = dis(_x2, _y2, _x3, _y3);
+
     		return _a + _b + _c;
     	}
 
-    	bool test()
+    	bool isTriangle() const
     	{
+    		double _a = dis(_x1, _y1, _x2, _y2);
+    	    double _b = dis(_x1, _y1, _x3, _y3);
+    	    double _c = dis(_x2, _y2, _x3, _y3);
+
     		if ((_a+_b>=_c)and(_a+_c>=_b)and(_b+_c>=_a))
     		{
     			return true;
@@ -31,12 +39,23 @@ class Triangle
     	}
 
     private:
-    	double _a;
-    	double _b;
-    	double _c;
+    	double _x1; double _y1;
+    	double _x2; double _y2;
+    	double _x3; double _y3;
+
+    	double dis(double a, double b, double c, double d) const
+    	{
+    		double i = pow((a-c), 2);
+    		double j = pow((c-d), 2);
+            return sqrt(i+j);
+    	}
 
     	double heron() const
     	{
+    		double _a = dis(_x1, _y1, _x2, _y2);
+    	    double _b = dis(_x1, _y1, _x3, _y3);
+    	    double _c = dis(_x2, _y2, _x3, _y3);
+
     		double s = (_a + _b + _c)/2;
     		return sqrt(s*(s-_a)*(s-_b)*(s-_c));
     	}
