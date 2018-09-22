908b8c8922b47153ad09a3e495105ba00dd0dcbd
jysh1214 <jyxemperor@gmail.com>
2018-09-22 11:47:55 +0800

    Update triangle.h

--- week1/triangle.h
+++ week1/triangle.h
@@ -5,7 +5,9 @@ class Triangle
     public:
         Triangle(double x1, double y1, double x2, double y2, double x3, double y3):_x1(x1), _x2(x2), _x3(x3), _y1(y1), _y2(y2), _y3(y3)
         {
-
+            _a = sqrt(pow((_x1-_x2),2)+pow((_y1-_y2),2));
+            _b = sqrt(pow((_x1-_x3),2)+pow((_y1-_y3),2));
+            _c = sqrt(pow((_x2-_x3),2)+pow((_y2-_y3),2));
         }
 
         double area() const
@@ -15,19 +17,11 @@ class Triangle
 
     	double perimeter() const
     	{
-    		double _a = dis(_x1, _y1, _x2, _y2);
-    	    double _b = dis(_x1, _y1, _x3, _y3);
-    	    double _c = dis(_x2, _y2, _x3, _y3);
-
     		return _a + _b + _c;
     	}
 
     	bool isTriangle() const
     	{
-    		double _a = dis(_x1, _y1, _x2, _y2);
-    	    double _b = dis(_x1, _y1, _x3, _y3);
-    	    double _c = dis(_x2, _y2, _x3, _y3);
-
     		if ((_a+_b>_c)and(_a+_c>_b)and(_b+_c>_a))
     		{
     			return true;
@@ -42,13 +36,10 @@ class Triangle
     	double _x1; double _y1;
     	double _x2; double _y2;
     	double _x3; double _y3;
-
-    	double dis(double a, double b, double c, double d) const
-    	{
-    		double i = pow((a-c), 2);
-    		double j = pow((b-d), 2);
-            return sqrt(i+j);
-    	}
+    
+        double _a;
+        double _b;
+        double _c;
 
     	double heron() const
     	{
