61ccfae5af88dd6ec70beeacb3f22a28c65251ce
jysh1214 <jyxemperor@gmail.com>
2018-09-22 21:42:22 +0800

    Create ut_main.cpp

---
+++ week2/test/ut_main.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "ut_shape.h"
+#include "ut_sort.h"
+
+using namespace::std;
+
+int main(int argc, char ** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
