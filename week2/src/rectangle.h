92e71f33a2bf07aff10b70346e3b7d78fb971a0b
jysh1214 <jyxemperor@gmail.com>
2018-09-22 15:00:17 +0800

    Create rectangle.h

---
+++ week2/src/rectangle.h
@@ -0,0 +1,62 @@
+#include<iostream>
+
+using namespace::std;
+
+class Rectangle: public Shape
+{
+    public:
+        Rectangle(double l, double w):_l(l), _w(w)
+        {
+
+        }
+
+        double area() const
+        {
+            return _l * _w;
+        }
+
+        double perimeter() const
+        {
+            return 2 * (_l + _w);
+        }
+
+        double compactness() const
+        {
+            try
+            {
+                return SafeDivide(pow(perimeter(), 2), area());
+            } 
+            catch (const invalid_argument& e)
+            {
+                cout<<"Caught exception:"<<e.what()<<endl;
+            }
+        }
+
+        bool isRectangle()
+        {
+            if ((_l<=0)or(_w<=0))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    private:
+        double _l;
+        double _w;
+
+        double SafeDivide(double num, double den) const
+        {
+            if(den==0)
+            {
+                throw invalid_argument("Division by zero.");
+            }
+            else
+            {
+                return num / den;
+            }
+        }
+};
