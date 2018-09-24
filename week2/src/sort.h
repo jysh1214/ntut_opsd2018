b07a02b007dcd036abae724bddd70717e75f9820
jysh1214 <jyxemperor@gmail.com>
2018-09-25 00:38:09 +0800

    Update sort.h

--- week2/src/sort.h
+++ week2/src/sort.h
@@ -2,38 +2,39 @@
 #include <vector>
 
 using namespace::std;
-template<class T> 
-void sortArea(T);
+template<class T> void sortArea(T);
+template<class T> void sortPerimeter(T);
 
 class Sort
 {
-	public:
-		Sort(vector<Shape*> *v):_v(v)
-		{
+    public:
+        Sort(vector<Shape*> *v):_v(v)
+        {
 
-		}
+        }
 
-		//The argument ** Accept comparsion ** can accept lambda, function, and object
-		//You should implement area ascending and descending lamda function as parameter for Sort::sortArea()
-		template<class T> 
-		void sortArea(T comparsion)
-		{
+        //The argument ** Accept comparsion ** can accept lambda, function, and object
+        //You should implement area ascending and descending lamda function as parameter for Sort::sortArea()
+        template<class T> 
+        void sortArea(T comparsion)
+        {
             sort(_v->begin(), _v->end(), comparsion);
-		}
-		/*
+        }
+        
         //You should use those functions as parameter for Sort::sortPerimeter()
-		void sortPerimeter(T comparsion)
-		{
+        template<class T> 
+        void sortPerimeter(T comparsion)
+        {
             sort(_v->begin(), _v->end(), comparsion);
-		}
+        }
+
         //You should use those objects as parameter for Sort::sortCompactness()
-		void sortCompactness(T comparsion)
-		{
+        template<class T> 
+        void sortCompactness(T comparsion)
+        {
             sort(_v->begin(), _v->end(), comparsion);
-		}
-		*/
+        }
 
-	private:
-		vector<Shape*>*_v;
+    private:
+        vector<Shape*>*_v;
 };
-
