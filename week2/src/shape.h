6bce46449b6163dceb6986925fb19e4c2139403a
jysh1214 <jyxemperor@gmail.com>
2018-09-26 10:24:11 +0800

    Update shape.h

--- week2/src/shape.h
+++ week2/src/shape.h
@@ -1,7 +1,21 @@
+#ifndef _SHAPE_H
+#define _SHAPE_H
+
 class Shape
 {
     public:
         virtual double area() const = 0;
         virtual double perimeter() const = 0;
-        virtual double compactness() const = 0;
+        virtual double compactness()
+        {
+        	if (this->area()==0)
+        	{
+        		throw std::string("Division by zero");
+        	}
+        	else
+        	{
+        		return (this->perimeter()*this->perimeter())/this->area();
+        	}
+        }
 };
+#endif
