110815466066287c2176103644af4a22b839877a
jysh1214 <jyxemperor@gmail.com>
2018-10-14 01:09:33 +0800

    Create file.h

---
+++ week4/fs/src/file.h
@@ -0,0 +1,15 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include "node.h"
+
+class File: public Node
+{
+    public:
+        
+        File(const char * path): Node(path)
+        {
+
+        }
+};
+#endif
