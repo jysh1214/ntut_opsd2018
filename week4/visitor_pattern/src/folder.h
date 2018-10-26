a4d45f2f05db0ac1beb6bf0b85f048fd17c2a19c
jysh1214 <jyxemperor@gmail.com>
2018-10-26 17:19:55 +0800

    Update folder.h

--- week4/visitor_pattern/src/folder.h
+++ week4/visitor_pattern/src/folder.h
@@ -14,6 +14,7 @@ class Folder: public Node
         friend class Action;
         friend class Name;
         friend class Find;
+        friend class InfoContent;
 
         Folder(const char *path): Node(path)
         {
@@ -25,73 +26,27 @@ class Folder: public Node
             _children.push_back(node);
         }
 
-        int infoContent() const
-        {
-            int sum = 0;
-            for (int i = 0; i < _children.size(); i++)
-            {
-                sum += _children[i]->infoContent();
-            }
-            return sum;
-        }
-
         int numberOfChildren() const
         {
             return _children.size();
         }
 
-        // string find(string nodeName) const
-        // {
-        //     vector<Node *> totalFind;
-        //     string findList = "";
-        //     string temp = "";
-
-        //     this->findName(nodeName, totalFind);
-
-        //     if (totalFind.size()==0) return "";
-            
-        //     for (int i = 0; i < totalFind.size(); i++)
-        //     { 
-        //         int x = (this->getPath()).size();
-        //         temp += ".";
-        //         temp += (totalFind[i]->getPath()).substr(x, (totalFind[i]->getPath()).size());
-        //         findList += temp; 
-        //         temp.clear();
-        //         if (i!=totalFind.size()-1) findList+= '\n';
-        //     }
-        //     return findList;
-        // }
-
-        void findName (string nodeName, vector<Node *>& answer) //const
+        void accept(Action *visitor)
         {
-            for (int i = 0; i < _children.size(); i++)
-            { 
-                Node *node = this->_children[i];
-                if (node->name() == nodeName) 
-                {
-                    answer.push_back(node);
-                }
-                node->findName(nodeName, answer);
-            }
+            visitor->visitFolder(this);
         }
 
-        string accept(Action *visitor)
-        {
-            return visitor->visitFolder(this);
-        }
-
-        vector<Node *> getChildren()
+        vector<Node *> getChildren() const
         {
             return _children;
         }
 
-        string classType()
+        string classType() const
         {
             return "Folder";
         }
         
     private:
         vector<Node *> _children;
- 
 };
 #endif
