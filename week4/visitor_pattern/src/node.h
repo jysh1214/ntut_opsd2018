3a83154199f8d2ed4af469b8dcd3471759034453
jysh1214 <jyxemperor@gmail.com>
2018-10-26 17:22:22 +0800

    Update node.h

--- week4/visitor_pattern/src/node.h
+++ week4/visitor_pattern/src/node.h
@@ -16,21 +16,18 @@ class Node
     public:
         friend class Action;
         friend class Name;
+        friend class Find;
+        friend class InfoContent;
 
         Node(const char * path): _path(path)
         {
             lstat(_path, &_st);
-            if (lstat(_path, &_st) == -1) throw std::string("No such file or directory.");
+            if (lstat(_path, &_st) == -1) throw string("No such file or directory.");
         }
 
         int size() const
         {
             return _st.st_size;
-        }
-
-        virtual int infoContent() const
-        {
-            return size();
         }
 
         virtual void add(Node *node)
@@ -48,35 +45,21 @@ class Node
             return _path;
         }
 
-        string name() const
+        virtual void accept(Action *visitor)
         {
-            string nodeName;
-            string tempStr = this->_path;
-            nodeName = tempStr.substr(tempStr.find_last_of("/")+1, tempStr.length()-tempStr.find_last_of("/"));
-
-            return nodeName;
+            // Name
+            // Find
+            // InfoContent
         }
 
-        // virtual string find(string nodeName) const = 0;
-
-        virtual void findName(string nodeName, std::vector<Node *>& answer) //const
+        virtual vector<Node *> getChildren() const
         {
 
         }
 
-        virtual string accept(Action *visitor)
+        virtual string classType() const
         {
 
-        }
-
-        virtual vector<Node *> getChildren()
-        {
-
-        }
-
-        virtual string classType()
-        {
-            
         }
 
   private:
