67d50c634f619a949ae5956ab5357524a2fad408
jysh1214 <jyxemperor@gmail.com>
2018-10-26 17:09:01 +0800

    Update action.cpp

--- week4/visitor_pattern/src/action.cpp
+++ week4/visitor_pattern/src/action.cpp
@@ -3,20 +3,10 @@
 #include "file.h"
 #include "folder.h"
 
-Action::Action()
-{
+Action::Action(){}
+void Action::visitFile(File *file){}
+void Action::visitFolder(Folder *folder){}
 
-}
-/* error: ‘virtual’ outside class declaration
-You only need to specify it in the class definition (in the header file). 
-After that, the compiler knows that it's virtual.
-*/
-string Action::visitFile(File *file)
-{
-
-}
-
-string Action::visitFolder(Folder *folder)
-{
-
-}
+string Action::getName(){}
+string Action::findResult(){};
+int Action::getContentSize(){};
