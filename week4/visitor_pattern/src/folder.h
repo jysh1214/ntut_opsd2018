225294b2a1bf24ad910fe93ce7b57f1b4a1a705b
jysh1214 <jyxemperor@gmail.com>
2018-11-02 16:13:04 +0800

    Update folder.h

--- week4/visitor_pattern/src/folder.h
+++ week4/visitor_pattern/src/folder.h
@@ -1,20 +1,53 @@
 #ifndef FOLDER_H
 #define FOLDER_H
 
+#include <memory>
 #include <vector>
+#include <string>
 #include "node.h"
 #include "action.h"
-#include "name.h"
+#include "node_iterator.h"
 
 using namespace std;
 
 class Folder: public Node
 {
     public:
-        friend class Action;
-        friend class Name;
-        friend class Find;
-        friend class InfoContent;
+        class FolderIterator: public NodeIterator
+        {
+            public:
+                FolderIterator(Folder *f): _folder(f)
+                {
+
+                }
+
+                void first()
+                {
+                    _it = _folder->_children.begin();
+                }
+
+                Node *currentItem()
+                {
+                    if (isDone()) throw string("no current item.");
+                    return *_it;
+                }
+
+                void next()
+                {
+                    if (isDone()) throw string("moving past the end");
+                    _it++;
+                }
+
+                bool isDone()
+                {
+                    return (_it == _folder->_children.end());
+                }
+
+            private:
+                Folder *_folder;
+                vector<Node*>::iterator _it;
+        };
+
 
         Folder(const char *path): Node(path)
         {
@@ -36,14 +69,14 @@ class Folder: public Node
             visitor->visitFolder(this);
         }
 
+        NodeIterator * createIterator()
+        {
+            return new FolderIterator(this);
+        }
+
         vector<Node *> getChildren() const
         {
             return _children;
-        }
-
-        string classType() const
-        {
-            return "Folder";
         }
         
     private:
