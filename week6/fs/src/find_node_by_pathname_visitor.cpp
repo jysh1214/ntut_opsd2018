2ce4975df124d537a6e28f93b2e3c0f021811f9d
jysh1214 <jyxemperor@gmail.com>
2018-11-29 21:43:15 +0800

    Create find_node_by_pathname_visitor.cpp

---
+++ week6/fs/src/find_node_by_pathname_visitor.cpp
@@ -0,0 +1,25 @@
+#include "find_node_by_pathname_visitor.h"
+#include "node.h"
+#include "file.h"
+#include "folder.h"
+#include "link.h"
+
+void FindNodeByPathnameVisitor::visitFolder(Folder * folder)
+{
+
+}
+
+void FindNodeByPathnameVisitor::visitFile(File * file)
+{
+
+}
+
+void FindNodeByPathnameVisitor::visitLink(Link * link)
+{
+
+}
+
+Node * FindNodeByPathnameVisitor::getNode() const
+{
+
+}
