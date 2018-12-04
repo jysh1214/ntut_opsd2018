bb15795f84351f77038de5670877a74561a81565
jysh1214 <jyxemperor@gmail.com>
2018-12-04 13:22:42 +0800

    Create node_visitor.h

---
+++ week7/src/node_visitor.h
@@ -0,0 +1,25 @@
+#ifndef NODE_VISITOR_H
+#define NODE_VISITOR_H
+
+#include <string>
+#include <vector>
+
+class Node;
+class File;
+class Folder;
+class Link;
+
+class NodeVisitor
+{
+  public:
+    virtual void visitFile(File *file) = 0;
+    virtual void visitFolder(Folder *folder) = 0;
+    virtual void visitLink(Link *link) = 0;
+
+    virtual int getContentSize() {}
+    virtual std::string findResult() {}
+    virtual std::vector<Node *> getLinks() {}
+    virtual Node * getNode() {}
+};
+
+#endif
