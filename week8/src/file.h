cd6ba56e4817b342ded1f2fa5a27bc5768da5df7
jysh1214 <jyxemperor@gmail.com>
2018-12-23 22:23:14 +0800

    Create file.h

---
+++ week8/src/file.h
@@ -0,0 +1,25 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include "node.h"
+
+class File : public Node
+{
+public:
+    File(const char *path) : Node(path)
+    {
+        struct stat st;
+        if (lstat(path, &st) == 0)
+        {
+            if (!S_ISREG(st.st_mode))
+            {
+                throw std::string("Node type error.");
+            }
+        }
+    }
+
+    virtual ~File(){}
+
+};
+
+#endif
