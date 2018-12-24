469ab961681869bbf1de83c68045d46ceb7c7577
jysh1214 <jyxemperor@gmail.com>
2018-12-24 18:07:15 +0800

    Update node.h

--- week8/src/node.h
+++ week8/src/node.h
@@ -1,34 +1,41 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <wx/wx.h>
 #include <sys/stat.h>
 #include <string>
 #include <vector>
 
+#include <iostream>
+using namespace std;
+
 class Node
 {
 public:
-    Node(const char *path) : _path(path)
+    Node(char *path) : _path(path)
     {
         lstat(_path, &_st);
         if (lstat(_path, &_st) == -1)
         {
             throw std::string("No such file or directory.");
         }
+
+        std::string tempStr = _path;
+
+        _name= tempStr.substr(tempStr.find_last_of("/") + 1, 
+        tempStr.length() - tempStr.find_last_of("/"));
     }
+
     virtual ~Node(){}
 
-    std::string getPath() const
-    {
+    std::string getPath()
+    {  
         return _path;
     }
 
-    std::string name() const
+    std::string name()
     {
-        std::string nodeName;
-        std::string tempStr = this->_path;
-        nodeName = tempStr.substr(tempStr.find_last_of("/") + 1, tempStr.length() - tempStr.find_last_of("/"));
-        return nodeName;
+        return _name;
     }
 
     int size() const
@@ -47,7 +54,8 @@ public:
     }
 
 private:
-    const char *_path;
+    char *_path;
+    std::string _name;
     struct stat _st;
 };
 
